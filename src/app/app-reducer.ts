1bde4acb04c0f0ce0e005e31b8e9051ce0a4d638
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-05-07 13:03:44 +0000

    3. Добавил SET_APP_ERROR

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -9,6 +9,7 @@ type AppStateType = typeof initialState
 
 // Action Type
 const SET_APP_STATUS = 'app/SET_APP_STATUS' as const
+const SET_APP_ERROR = 'app/SET_APP_ERROR' as const
 
 export const setAppStatusAC = (status: RequestStatusType) => {
   return {
@@ -17,15 +18,25 @@ export const setAppStatusAC = (status: RequestStatusType) => {
   }
 }
 
+export const setErrorAC = (error: string | null) => ({
+  type: SET_APP_ERROR,
+  payload: { error },
+} as const)
+
 export const appReducer = (state: AppStateType = initialState, action: ActionsType): AppStateType => {
   switch (action.type) {
     case SET_APP_STATUS:
       return  {
         ...state, status: action.payload.status
       }
+    case SET_APP_ERROR:
+      return {
+        ...state,
+        error: action.payload.error,
+      }
     default:
       return state
   }
 }
 
-type ActionsType = ReturnType<typeof setAppStatusAC>
+type ActionsType = ReturnType<typeof setAppStatusAC> | ReturnType<typeof setErrorAC>
