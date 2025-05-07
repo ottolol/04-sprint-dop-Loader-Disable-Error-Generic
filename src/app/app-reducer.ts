3c73ab7532627b837cd6b9a791051990fb837cf2
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-05-07 12:01:40 +0000

    1. Добавил setAppStatusAC, case в appReducerб протипизировал ActionsType

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -7,11 +7,25 @@ const initialState = {
 
 type AppStateType = typeof initialState
 
+// Action Type
+const SET_APP_STATUS = 'app/SET_APP_STATUS' as const
+
+export const setAppStatusAC = (status: RequestStatusType) => {
+  return {
+    type: SET_APP_STATUS,
+    payload: { status } as const
+  }
+}
+
 export const appReducer = (state: AppStateType = initialState, action: ActionsType): AppStateType => {
   switch (action.type) {
+    case SET_APP_STATUS:
+      return  {
+        ...state, status: action.payload.status
+      }
     default:
       return state
   }
 }
 
-type ActionsType = any
+type ActionsType = ReturnType<typeof setAppStatusAC>
