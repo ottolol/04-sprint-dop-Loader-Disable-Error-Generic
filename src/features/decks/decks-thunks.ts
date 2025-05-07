74a5a98c7b26676311fc586f53a767d81ac881a5
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-05-07 12:00:41 +0000

    1. Переписал fetchDecksTC, addDeckTC на async, await и try, catch

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -1,17 +1,46 @@
 import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
+import { setAppStatusAC } from '../../app/app-reducer.ts'
 
-export const fetchDecksTC = () => (dispatch: Dispatch) => {
-  decksAPI.fetchDecks().then((res) => {
-    dispatch(setDecksAC(res.data.items))
-  })
+// export const fetchDecksTC = () => (dispatch: Dispatch) => {
+//   decksAPI.fetchDecks().then((res) => {
+//     dispatch(setAppStatusAC( 'loading' ))
+//     dispatch(setDecksAC(res.data.items))
+//   })
+// }
+export const fetchDecksTC = () => async (dispatch: Dispatch) => {
+  try {
+    dispatch(setAppStatusAC('loading'));
+    const res = await decksAPI.fetchDecks();
+    dispatch(setDecksAC(res.data.items));
+    dispatch(setAppStatusAC('succeeded'));
+  } catch (error) {
+    // обработка ошибок, например:
+    console.error('Ошибка при загрузке колод:', error);
+    
+    // можно вызвать setErrorAC или другие экшены для отображения ошибки
+    // dispatch(setErrorAC(error.message));
+    
+    // также можно установить статус 'failed'
+    // dispatch(setAppStatusAC('failed'));
+  }
 }
 
+// export const addDeckTC = (name: string) => async (dispatch: Dispatch) => {
+//   return decksAPI.addDeck(name).then((res) => {
+//     dispatch(addDeckAC(res.data))
+//   })
+// }
 export const addDeckTC = (name: string) => async (dispatch: Dispatch) => {
-  return decksAPI.addDeck(name).then((res) => {
+  try {
+    dispatch(setAppStatusAC('loading'))
+    const res = await decksAPI.addDeck(name)
     dispatch(addDeckAC(res.data))
-  })
+    dispatch(setAppStatusAC('succeeded'))
+  } catch (error) {
+    console.error('Ошибка при добавлении колод:', error);
+  }
 }
 
 export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
